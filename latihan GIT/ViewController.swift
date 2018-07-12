f178af2c0d09b0f5d093a49b8cac3ad4045909ee
Raditya Kottama <kottamaraditya@yahoo.com>
2018-07-12 23:46:22 +0700

    test

--- latihan GIT/ViewController.swift
+++ latihan GIT/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("start")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
